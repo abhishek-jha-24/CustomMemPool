2ea81e90f23af60ab6c3d61931ed53265de4c0d3
abhishek-jha-24 <abhi2004jha@gmail.com>
2025-07-18 11:19:18 +0530

    added smart counts for managining on multi-threaded level | fixed bugs in dealloction

--- main.cpp
+++ main.cpp
@@ -1,122 +1,155 @@
 #include <iostream>
+#include <atomic>
+#include <thread>
+
 using namespace std;
 
 template<typename T>
-class MemoryPoolNode {
+class MempoolNode {
 public:
     T data;
-    MemoryPoolNode* next;
-    MemoryPoolNode* nextFree;
+    MempoolNode* next;
+    MempoolNode* bFree;
+    std::atomic<int> smartCount;
 
-    MemoryPoolNode() {
+    MempoolNode() : next(nullptr), bFree(nullptr), smartCount(1) {
         cout << "Creating node..." << endl;
     }
 
-    ~MemoryPoolNode() {
+    ~MempoolNode() {
         cout << "Deallocating node..." << endl;
         next = nullptr;
-        nextFree = nullptr;
+        bFree = nullptr;
+    }
+};
+
+class SpinLock {
+    std::atomic_flag flag = ATOMIC_FLAG_INIT;
+public:
+    void lock() {
+        while (flag.test_and_set(std::memory_order_acquire));
+    }
+
+    void unlock() {
+        flag.clear(std::memory_order_release);
     }
 };
 
 template <typename T>
-class MemoryPool {
+class Mempool {
 private:
     int totalNodes;
-    MemoryPoolNode<T>* poolHead = nullptr;
-    MemoryPoolNode<T>* freeListHead = nullptr;
+    MempoolNode<T>* head = nullptr;
+    MempoolNode<T>* freeNodes = nullptr;
+    SpinLock spinLock;
 
 public:
-    MemoryPool(int totalNodes_) : totalNodes(totalNodes_) {
+    Mempool(int count) : totalNodes(count) {
         for (int i = 0; i < totalNodes; i++) {
-            cout << "Creating node " << i << endl;
-            MemoryPoolNode<T>* newNode = new MemoryPoolNode<T>();
-            newNode->next = poolHead;
-            newNode->nextFree = poolHead;
-            poolHead = newNode;
+            auto* newNode = new MempoolNode<T>();
+            newNode->next = head;
+            newNode->bFree = head;
+            head = newNode;
         }
-        freeListHead = poolHead;
+        freeNodes = head;
+    }
+
+    ~Mempool() {
+        MempoolNode<T>* current = head;
+        while (current) {
+            current->data.~T();
+            MempoolNode<T>* temp = current;
+            current = current->next;
+            delete temp;
+        }
     }
 
     void checkSize() {
-        cout << "Checking number of linked list elements..." << endl;
-        MemoryPoolNode<T>* current = poolHead;
-        while (current) {
-            cout << "Walking through list..." << endl;
-            current = current->next;
+        cout << "Checking linked list size..." << endl;
+        MempoolNode<T>* temp = head;
+        while (temp) {
+            cout << " Node active..." << endl;
+            temp = temp->next;
         }
     }
 
     T* allocate() {
-        if (!freeListHead) {
-            cout << "No free nodes available for allocation." << endl;
+        spinLock.lock();
+        if (!freeNodes) {
+            spinLock.unlock();
+            cout << "No more nodes available." << endl;
             return nullptr;
         }
 
-        MemoryPoolNode<T>* allocatedNode = freeListHead;
-        freeListHead = freeListHead->nextFree;
+        MempoolNode<T>* node = freeNodes;
+        freeNodes = freeNodes->bFree;
+        node->smartCount.store(1);
+        spinLock.unlock();
+
         cout << "Allocation successful!" << endl;
-        return &(allocatedNode->data);
+        return &(node->data);
+    }
+
+    void share(T* ptr) {
+        auto* node = reinterpret_cast<MempoolNode<T>*>(ptr);
+        node->smartCount.fetch_add(1);
+        cout << "Sharing node. Smart count: " << node->smartCount.load() << endl;
     }
 
     void deallocate(T* ptr) {
-        MemoryPoolNode<T>* nodeToFree = (MemoryPoolNode<T>*) ptr;
+        auto* node = reinterpret_cast<MempoolNode<T>*>(ptr);
+        int count = node->smartCount.fetch_sub(1) - 1;
 
-        cout << "Starting deallocation..." << endl;
-        nodeToFree->nextFree = freeListHead;
-        freeListHead = nodeToFree;
-        cout << "Deallocation successful!" << endl;
-    }
-
-    ~MemoryPool() {
-        cout << "Destroying memory pool..." << endl;
-        MemoryPoolNode<T>* current = poolHead;
-        MemoryPoolNode<T>* temp;
-
-        while (current) {
-            current->data.~T();  // Manually call destructor for contained objects
-            temp = current;
-            current = current->next;
-            delete temp;
+        if (count == 0) {
+            spinLock.lock();
+            node->bFree = freeNodes;
+            freeNodes = node;
+            spinLock.unlock();
+            cout << "Deallocation successful." << endl;
+        } else {
+            cout << "Node not deallocated. Smart count: " << count << endl;
         }
     }
 };
 
 class OrderResponse {
 public:
-    int timestamp;
+    int ts;
     int successStatus;
 
     OrderResponse() = default;
 
-    OrderResponse(int ts, int status) {
-        timestamp = ts;
-        successStatus = status;
-    }
+    OrderResponse(int ts_, int status_) : ts(ts_), successStatus(status_) {}
 
     void print() const {
-        cout << "Timestamp: " << timestamp << ", Success Status: " << successStatus << endl;
+        cout << "Timestamp: " << ts << ", Success Status: " << successStatus << endl;
     }
 
     ~OrderResponse() {
-        cout << "Destroyed OrderResponse" << endl;
+        cout << "Destructed OrderResponse" << endl;
     }
 };
 
 int main() {
-    MemoryPool<OrderResponse> pool(28);
+    Mempool<OrderResponse> pool(5);
     pool.checkSize();
 
     OrderResponse* or1 = new (pool.allocate()) OrderResponse(1, 1);
     OrderResponse* or2 = new (pool.allocate()) OrderResponse(2, 2);
+
     or1->print();
     or2->print();
 
-    pool.deallocate(or1);
+    pool.share(or1); // simulate sharing with another thread
+
+    pool.deallocate(or1); // ref count goes from 2 to 1, not deallocated
+    pool.deallocate(or1); // ref count 0, deallocated
 
     OrderResponse* or3 = new (pool.allocate()) OrderResponse(3, 3);
-    or2->print();
     or3->print();
+
+    pool.deallocate(or2);
+    pool.deallocate(or3);
 
     return 0;
 }
